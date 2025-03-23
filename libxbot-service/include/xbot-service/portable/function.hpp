c6cc75f82c6b74fe962ebb3b489c1cc98a4ecf6d
Robert Vollmer <dev@robv.de>
2025-03-23 21:41:38 +0000

    Add "function" portable

---
+++ libxbot-service/include/xbot-service/portable/function.hpp
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_HPP
+#define FUNCTION_HPP
+
+#include <xbot/function_impl.hpp>
+
+#ifndef XBOT_FUNCTION_TYPEDEF
+#error XBOT_FUNCTION_TYPEDEF undefined
+#endif
+
+#endif  // FUNCTION_HPP
