9b4632cfadcfae5e1184a173defc9858fe084929
Robert Vollmer <dev@robv.de>
2025-04-11 01:14:34 +0000

    Revert "added etl as dependency, added timer example"
    This reverts commit 9b4cb11f6851a79c5ee84e8520b5d5d039958ec0.

--- examples/services/EchoService/EchoService.hpp
+++ examples/services/EchoService/EchoService.hpp
@@ -6,28 +6,18 @@
 #define ECHOSERVICE_HPP
 
 #include "EchoServiceBase.hpp"
-#include "etl/callback_timer.h"
 
 class EchoService : public EchoServiceBase {
  public:
-  explicit EchoService(uint16_t service_id) : EchoServiceBase(service_id, 1'000) {
+  explicit EchoService(uint16_t service_id) : EchoServiceBase(service_id, 1'000'000) {
   }
 
  private:
   void tick() override;
 
-void timer_callback1();
-void timer_callback2();
-
   uint32_t echo_count = 0;
 
-  etl::callback_timer<5> callback_timer_{};
-
-  etl::delegate<void()> timer1_delegate_;
-  etl::delegate<void()> timer2_delegate_;
-
  protected:
-  void OnCreate() override;
   void OnInputTextChanged(const char *new_value, uint32_t length) override;
   bool OnStart() override;
   void OnStop() override;
