aea06cb45734b04a973395f73ba19add7dac05ca
Robert Vollmer <dev@robv.de>
2025-05-09 22:37:33 +0000

    Refactor AddService(Interface).cmake

--- codegen/cmake/AddService.cmake
+++ codegen/cmake/AddService.cmake
@@ -1,24 +1,27 @@
 function(add_service SERVICE_NAME JSON_FILE)
-    add_library(${SERVICE_NAME} OBJECT EXCLUDE_FROM_ALL
-    )
+    add_library(${SERVICE_NAME} OBJECT EXCLUDE_FROM_ALL)
     target_add_service(${SERVICE_NAME} ${SERVICE_NAME} ${JSON_FILE})
 endfunction()
 
 function(target_add_service TARGET_NAME SERVICE_NAME JSON_FILE)
     # generate the output directory, otherwise python will complain when multiple instances are run
     file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/generated/include)
+
+    set(COG_BASE_ARGS -m cogapp -d -I ${XBOT_CODEGEN_PATH}/xbot_codegen -Dservice_file=${JSON_FILE})
+    set(HEADER_TEMPLATE ${XBOT_CODEGEN_PATH}/templates/ServiceTemplate.hpp)
+    set(SOURCE_TEMPLATE ${XBOT_CODEGEN_PATH}/templates/ServiceTemplate.cpp)
+    set(HEADER_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/generated/include/${SERVICE_NAME}Base.hpp)
+    set(SOURCE_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/generated/${SERVICE_NAME}Base.cpp)
+
     add_custom_command(
-            OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/generated/include/${SERVICE_NAME}Base.hpp ${CMAKE_CURRENT_BINARY_DIR}/generated/${SERVICE_NAME}Base.cpp
-            COMMAND ${Python3_EXECUTABLE} -m cogapp -d -I ${XBOT_CODEGEN_PATH}/xbot_codegen -D service_file=${JSON_FILE} -o ${CMAKE_CURRENT_BINARY_DIR}/generated/include/${SERVICE_NAME}Base.hpp ${XBOT_CODEGEN_PATH}/templates/ServiceTemplate.hpp
-            COMMAND ${Python3_EXECUTABLE} -m cogapp -d -I ${XBOT_CODEGEN_PATH}/xbot_codegen -D service_file=${JSON_FILE} -o ${CMAKE_CURRENT_BINARY_DIR}/generated/${SERVICE_NAME}Base.cpp ${XBOT_CODEGEN_PATH}/templates/ServiceTemplate.cpp
-            DEPENDS ${XBOT_CODEGEN_PATH}/templates/ServiceTemplate.hpp ${XBOT_CODEGEN_PATH}/templates/ServiceTemplate.cpp ${JSON_FILE}
-            COMMENT "Generating code for service ${SERVICE_NAME}."
+        OUTPUT ${HEADER_OUTPUT} ${SOURCE_OUTPUT}
+        COMMAND ${Python3_EXECUTABLE} ${COG_BASE_ARGS} -o ${HEADER_OUTPUT} ${HEADER_TEMPLATE}
+        COMMAND ${Python3_EXECUTABLE} ${COG_BASE_ARGS} -o ${SOURCE_OUTPUT} ${SOURCE_TEMPLATE}
+        DEPENDS ${HEADER_TEMPLATE} ${SOURCE_TEMPLATE} ${XBOT_CODEGEN_PATH}/xbot_codegen/xbot_codegen.py ${JSON_FILE}
+        COMMENT "Generating code for service ${SERVICE_NAME}."
     )
 
-    target_sources(${TARGET_NAME} PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/generated/${SERVICE_NAME}Base.cpp
-            ${CMAKE_CURRENT_BINARY_DIR}/generated/include/${SERVICE_NAME}Base.hpp
-    )
-
+    target_sources(${TARGET_NAME} PRIVATE ${SOURCE_OUTPUT} ${HEADER_OUTPUT})
     target_include_directories(${TARGET_NAME} PUBLIC ${CMAKE_CURRENT_BINARY_DIR}/generated/include)
     target_link_libraries(${TARGET_NAME} PUBLIC xbot-service)
 endfunction()
