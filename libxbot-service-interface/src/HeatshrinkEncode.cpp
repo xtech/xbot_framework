bd70f29b5614e6701763ac788455c8cf829c4a05
Robert Vollmer <dev@robv.de>
2025-05-24 23:58:45 +0200

    Use static alloc heatshrink encoder
    They don't mix well if compiled both into one static library.

--- libxbot-service-interface/src/HeatshrinkEncode.cpp
+++ libxbot-service-interface/src/HeatshrinkEncode.cpp
@@ -5,24 +5,25 @@ extern "C" {
 }
 
 std::vector<uint8_t> HeatshrinkEncode(uint8_t *data, const size_t size) {
-  heatshrink_encoder *encoder = heatshrink_encoder_alloc(8, 4);
+  heatshrink_encoder encoder;
   std::vector<uint8_t> out;
   uint8_t buf[128];
   size_t processed = 0, processed_now, polled_now;
   HSE_poll_res poll_res;
 
+  heatshrink_encoder_reset(&encoder);
   while (processed < size) {
-    heatshrink_encoder_sink(encoder, &data[processed], size - processed, &processed_now);
+    heatshrink_encoder_sink(&encoder, &data[processed], size - processed, &processed_now);
     processed += processed_now;
 
     do {
-      poll_res = heatshrink_encoder_poll(encoder, buf, sizeof(buf), &polled_now);
+      poll_res = heatshrink_encoder_poll(&encoder, buf, sizeof(buf), &polled_now);
       out.insert(out.end(), buf, buf + polled_now);
     } while (poll_res == HSER_POLL_MORE);
   }
 
-  while (heatshrink_encoder_finish(encoder) == HSER_FINISH_MORE) {
-    heatshrink_encoder_poll(encoder, buf, sizeof(buf), &polled_now);
+  while (heatshrink_encoder_finish(&encoder) == HSER_FINISH_MORE) {
+    heatshrink_encoder_poll(&encoder, buf, sizeof(buf), &polled_now);
     out.insert(out.end(), buf, buf + polled_now);
   }
 
