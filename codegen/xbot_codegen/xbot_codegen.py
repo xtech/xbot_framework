50a760c8caee9601d8ab258ecd781396df8168ce
Robert Vollmer <dev@robv.de>
2025-05-26 21:11:54 +0000

    Support 64 bit integers

--- codegen/xbot_codegen/xbot_codegen.py
+++ codegen/xbot_codegen/xbot_codegen.py
@@ -10,9 +10,11 @@ raw_encoding_valid_types = [
     "uint8_t",
     "uint16_t",
     "uint32_t",
+    "uint64_t",
     "int8_t",
     "int16_t",
     "int32_t",
+    "int64_t",
     "float",
     "double",
 ]
