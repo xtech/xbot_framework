84d937096fb3f483b1fdcf91a215f8381a310d65
Jörg Ebeling <joerg@ebeling.ws>
2026-04-08 18:00:10 +0200

    Fix pointer alignment

--- include/xbot/config.hpp
+++ include/xbot/config.hpp
@@ -20,7 +20,7 @@ static constexpr uint16_t max_service_count = 25;
 /**
  * Settings for remote logging
  */
-[[maybe_unused]] static const char* remote_log_multicast_address = "233.255.255.1";
+[[maybe_unused]] static const char *remote_log_multicast_address = "233.255.255.1";
 static constexpr uint16_t max_log_length = 255;
 
 /**
@@ -40,7 +40,7 @@ static constexpr uint16_t max_sd_entry_size = 51;
 // Make sure that we have at least space for a 64 bit integer.
 static_assert(max_sd_entry_size > sizeof(uint64_t));
 static_assert(max_sd_entry_size > max_service_name_length);
-[[maybe_unused]] static const char* sd_multicast_address = "233.255.255.0";
+[[maybe_unused]] static const char *sd_multicast_address = "233.255.255.0";
 static constexpr uint32_t sd_advertisement_interval_micros = 10'000'000;
 // the fast sd advertisement time is used as long as the service is not yet
 // claimed.
